9e018a096e31a3050dea244bd9425edf53f0f642
Wasim Khan <wasim.cg@gmail.com>
2020-08-24 12:15:39 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,25 +3,32 @@ import maya.api.OpenMaya as OpenMaya
 
 
 def setupPoleVectorConstraintWithNodes(ikHandle, poleVector):
+    """
+    setup pole vector constraint for an ikhandle with matrix nodes
+    Args:
+        ikHandle (str): maya ikhandle (RPSolver and ikSpringSolver) name
+        poleVector (str): pole vector control name
+
+    Returns:
+    """
     if not cmds.nodeType(ikHandle)=='ikHandle':
-        return OpenMaya.MGlobal.displayError('defined node is not ikHandle')
+        return OpenMaya.MGlobal.displayError('{node} is not an ikHandle'.format(node=ikHandle))
     startJoint = cmds.ikHandle(ikHandle, q=True, startJoint=True)
     
-    # first creating all necesary nodes needed for pole vetor math calculation
+    # First create all necessary nodes needed for pole vector math
     pos = cmds.createNode('pointMatrixMult', name='{node}_wpos_pmm'.format(node=startJoint))
     cm  = cmds.createNode('composeMatrix', name='{node}_wpos_cm'.format(node=startJoint))
     im  = cmds.createNode('inverseMatrix', name='{node}_wpos_im'.format(node=startJoint)) 
     mm  = cmds.createNode('multMatrix', name='{node}_pole_position_mm'.format(node=ikHandle)) 
     pole= cmds.createNode('pointMatrixMult', name='{node}_pole_position_pmm'.format(node=ikHandle)) 
     
-    # compose world space position for start joint
-    # since ikHandle is setting rotation value on startJoint
-    # I cant connect worldMatrix right away, it will create cycle
-    # Connecting postion attribute and parentMatrix will give us worldSpace position
+    # Since ikHandle is setting rotation value on startJoint, we can't connect worldMatrix right away.
+    # In order to avoid cycle, Compose world space position for start joint with pointMatrixMult node 
+    # Connecting position attribute and parentMatrix will give us worldSpace position
     cmds.connectAttr('{node}.translate'.format(node=startJoint), '{node}.inPoint'.format(node=pos))
     cmds.connectAttr('{node}.parentMatrix[0]'.format(node=startJoint), '{node}.inMatrix'.format(node=pos))
 
-    # Now composeMatrix from output so we can inverse and find local position from startJoint to pole control
+    # Now composeMatrix from output, so we can inverse and find local position from startJoint to pole control
     cmds.connectAttr('{node}.output'.format(node=pos), '{node}.inputTranslate'.format(node=cm))
     cmds.connectAttr('{node}.outputMatrix'.format(node=cm), '{node}.inputMatrix'.format(node=im))
     cmds.connectAttr('{node}.worldMatrix[0]'.format(node=poleVector), '{node}.matrixIn[0]'.format(node=mm))
